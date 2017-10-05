683d95a5f74b62dc25cecf3a15c033728f410e08
Alex <alexzhang1931@gmail.com>
2017-10-05 22:44:00 +1100

    Lookup postcode from Location services

--- COMP90018Proj/CrimeInfoViewController.swift
+++ COMP90018Proj/CrimeInfoViewController.swift
@@ -7,13 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CrimeInfoViewController: UIViewController {
+class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways{
+            print("no authorization")
+            return
+        }
+        
+        if !CLLocationManager.locationServicesEnabled() {
+            print("location serivice not available")
+            return
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        lookUpCurrentLocation(completionHandler: {(location: CLPlacemark?) in
+            print(location?.postalCode)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +46,21 @@ class CrimeInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?) -> Void) {
+        if let lastLocation = self.locationManager.location{
+            let geocoder = CLGeocoder()
+            
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: {(placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                } else {
+                    completionHandler(nil)
+                }
+            })
+        } else {
+            completionHandler(nil)
+        }
     }
-    */
-
+    
 }
