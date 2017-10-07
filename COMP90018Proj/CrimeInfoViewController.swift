d9f61b2631ff6b1650a94869989a97ae8988c8d0
Unknown <lisaqiu763@gmail.com>
2017-10-07 23:35:01 +1100

    add two pie charts
    add image view
    add two pie charts 
    one average, one from current location data.

--- COMP90018Proj/CrimeInfoViewController.swift
+++ COMP90018Proj/CrimeInfoViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 import CoreLocation
 import MapKit
-import Foundation
+import PieCharts
 import SwiftyJSON
 
-class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate {
+class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate,PieChartDelegate {
+    func onSelected(slice: PieSlice, selected: Bool) {
+        print("Selected:\(selected),slice:\(slice)")
+    }
+    
+    @IBOutlet weak var image2: UIImageView!
+    
+    
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
     
@@ -43,6 +50,8 @@ class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate {
 
         // show user locaiton on map
         mapView.showsUserLocation=true
+        
+        image2.image = UIImage(named: ("testImage"))
     }
 
     override func didReceiveMemoryWarning() {
@@ -118,9 +127,9 @@ class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate {
             let myjson = JSON(json)
             
             // count offence subdivision
-            var chartDataSuburb = [String:Int]()
+            var chartDataSuburb = [String:Double]()
             for temp in (myjson.array)!{
-                let key = temp["CSA Offence Subdivision"].string
+                let key = temp["CSA Offence Division"].string
                 if(chartDataSuburb[key!] != nil) {
                     let old = chartDataSuburb[key!]
                     let new = old! + 1
@@ -131,21 +140,134 @@ class CrimeInfoViewController: UIViewController, CLLocationManagerDelegate {
             }
             print(chartDataSuburb)
             print(chartDataSuburb.count)
+        
+//            self.pieChartView.layers = [self.createCustomViewsLayer(), self.createTextLayer()]
+//            self.pieChartView.delegate = self
+            self.pieChartView.models = self.createModels1(dic: chartDataSuburb)
+            self.pieChartViewA.models = self.createModels()
         }
         task.resume()
     }
     
-    // count the apperance of key in jsonString or jsonArray
-//    func countAppearance(JSONArray: Array<Any>){
-//        var counts:[String:Int] = [:]
-//        for item in JSONArray{
-//            counts[item] = counts[item,default:0]+=1
-//        }
-//        print(counts)
-//        
-//        for(key, value) in counts {
-//            print("\(key) occurs \(value) time(s)")
-//        }
-//    }
+    
+    @IBOutlet weak var pieChartView: PieChart!
+    @IBOutlet weak var pieChartViewA: PieChart!
+    override func viewDidAppear(_ animated: Bool) {
+        pieChartView.layers = [createCustomViewsLayer(), createTextLayer()]
+        pieChartView.delegate = self
+//        pieChartView.models = createModels() // order is important - models have to be set at the end
+        pieChartViewA.layers = [createCustomViewsLayer(), createTextLayer()]
+        pieChartViewA.delegate = self
+    }
+    
+
+    
+    // MARK: - Models
+ 
+    
+    fileprivate func createModels1(dic: Dictionary<String, Double>) -> [PieSliceModel] {
+        let alpha: CGFloat = 0.5
+        
+        return [
+            PieSliceModel(value: dic["A Crimes against the person"]!, color: UIColor.yellow.withAlphaComponent(alpha)),
+            PieSliceModel(value: dic["B Property and deception offences"]!, color: UIColor.blue.withAlphaComponent(alpha)),
+            PieSliceModel(value: dic["C Drug offences"]!, color: UIColor.green.withAlphaComponent(alpha)),
+            PieSliceModel(value: dic["D Public order and security offences"]!, color: UIColor.cyan.withAlphaComponent(alpha)),
+            PieSliceModel(value: dic["E Justice procedures offences"]!, color: UIColor.red.withAlphaComponent(alpha)),
+            PieSliceModel(value: dic["F Other offences"]!, color: UIColor.magenta.withAlphaComponent(alpha)),
+        ]
+    
+    }
+    
+    fileprivate func createModels() -> [PieSliceModel] {
+        let alpha: CGFloat = 0.5
+
+        return [
+            PieSliceModel(value: 1, color: UIColor.yellow.withAlphaComponent(alpha)),
+            PieSliceModel(value: 2, color: UIColor.blue.withAlphaComponent(alpha)),
+            PieSliceModel(value: 3, color: UIColor.green.withAlphaComponent(alpha)),
+            PieSliceModel(value: 4, color: UIColor.cyan.withAlphaComponent(alpha)),
+            PieSliceModel(value: 5, color: UIColor.red.withAlphaComponent(alpha)),
+            PieSliceModel(value: 6, color: UIColor.magenta.withAlphaComponent(alpha)),
+        ]
+
+    }
+
+    fileprivate func createCustomViewsLayer() -> PieCustomViewsLayer {
+        let viewLayer = PieCustomViewsLayer()
+        
+        let settings = PieCustomViewsLayerSettings()
+        settings.viewRadius = 135
+        settings.hideOnOverflow = false
+        viewLayer.settings = settings
+        
+        viewLayer.viewGenerator = createViewGenerator()
+        
+        return viewLayer
+    }
+    
+    fileprivate func createTextLayer() -> PiePlainTextLayer {
+        let textLayerSettings = PiePlainTextLayerSettings()
+        textLayerSettings.viewRadius = 60
+        textLayerSettings.hideOnOverflow = true
+        textLayerSettings.label.font = UIFont.systemFont(ofSize: 12)
+        
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 1
+        textLayerSettings.label.textGenerator = {slice in
+            return formatter.string(from: slice.data.percentage * 100 as NSNumber).map{"\($0)%"} ?? ""
+        }
+        
+        let textLayer = PiePlainTextLayer()
+        textLayer.settings = textLayerSettings
+        return textLayer
+    }
+    
+    fileprivate func createViewGenerator() -> (PieSlice, CGPoint) -> UIView {
+        return {slice, center in
+
+            let container = UIView()
+            container.frame.size = CGSize(width: 100, height: 40)
+            container.center = center
+            let view = UIImageView()
+            view.frame = CGRect(x: 30, y: 0, width: 40, height: 40)
+            container.addSubview(view)
+
+            if slice.data.id == 3 || slice.data.id == 0 {
+                let specialTextLabel = UILabel()
+                specialTextLabel.textAlignment = .center
+                if slice.data.id == 0 {
+                    specialTextLabel.text = "A"
+                    specialTextLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                } else if slice.data.id == 3 {
+                    specialTextLabel.textColor = UIColor.blue
+                    specialTextLabel.text = "B"
+                }
+                specialTextLabel.sizeToFit()
+                specialTextLabel.frame = CGRect(x: 0, y: 40, width: 100, height: 20)
+                container.addSubview(specialTextLabel)
+                container.frame.size = CGSize(width: 100, height: 60)
+            }
+
+
+//            // src of images: www.freepik.com, http://www.flaticon.com/authors/madebyoliver
+//            let imageName: String? = {
+//                switch slice.data.id {
+//                case 0: return "fish"
+//                case 1: return "grapes"
+//                case 2: return "doughnut"
+//                case 3: return "water"
+//                case 4: return "chicken"
+//                case 5: return "beet"
+//                case 6: return "cheese"
+//                default: return nil
+//                }
+//            }()
+//
+//            view.image = imageName.flatMap{UIImage(named: $0)}
+
+            return container
+        }
+    }
     
 }
