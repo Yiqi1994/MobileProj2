7c4e75afca9164ae38e6df68cd67301c11469b78
Alex <alexzhang1931@gmail.com>
2017-10-07 18:34:59 +1100

    Install pods, convert AZS into framework

--- COMP90018Proj/Bridging-Header.h
+++ COMP90018Proj/Bridging-Header.h
@@ -2,12 +2,12 @@
 //  Bridging-Header.h
 //  COMP90018Proj
 //
-//  Created by Kai Zhang on 5/10/17.
+//  Created by Kai Zhang on 7/10/17.
 //  Copyright © 2017 Unimelb. All rights reserved.
 //
 
 #ifndef Bridging_Header_h
 #define Bridging_Header_h
 
-#import <AZSClient/AZSClient.h>
+
 #endif /* Bridging_Header_h */
